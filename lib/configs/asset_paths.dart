bb70b2e3c3745bd29bd37167052cc3b1e3307534
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2023-12-06 21:46:39 +0100

    added required assets

---
+++ lib/configs/asset_paths.dart
@@ -0,0 +1,15 @@
+class AssetPaths {
+  // svg icons
+  static String homeIcon = 'assets/icons/home.svg';
+  static String momentsIcon = 'assets/icons/moments.svg';
+  static String postIcon = 'assets/icons/posts.svg';
+  static String profileIcon = 'assets/icons/profile.svg';
+  static String promoterIcon = 'assets/icons/promoter.svg';
+  static String searchIcon = 'assets/icons/search.svg';
+  static String tickIcon = 'assets/icons/tick.svg';
+  static String userIcon = 'assets/icons/user.svg';
+
+  // png images
+  static String logoImave = 'assets/images/logo.png';
+  static String welcomeImage = 'assets/images/welcome.png';
+}
