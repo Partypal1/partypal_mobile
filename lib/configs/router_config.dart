5c71b0cbf2a8f2511b394022c0b7206ca02f4b2a
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2023-12-28 21:44:39 +0100

    updated the router config

--- lib/configs/router_config.dart
+++ lib/configs/router_config.dart
@@ -2,11 +2,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
 import 'package:partypal/configs/route_paths.dart';
+import 'package:partypal/models/user_model.dart';
+import 'package:partypal/screens/authentication/forgot_password.dart';
 import 'package:partypal/screens/authentication/select_user_profile_screen.dart';
+import 'package:partypal/screens/authentication/sign_in_screen.dart';
+import 'package:partypal/screens/authentication/sign_up_screen.dart';
+import 'package:partypal/screens/authentication/verification.dart';
 import 'package:partypal/screens/onboarding_screen.dart';
 import 'package:partypal/screens/splash_screen.dart';
 
-final GoRouter routerConfig = GoRouter(
+final GoRouter routerConfig = GoRouter( // TODO: add routing animations
   initialLocation: RoutePaths.splashScreen,
   errorBuilder: (context, state) => const Placeholder(),
   routes: [
@@ -17,6 +22,7 @@ final GoRouter routerConfig = GoRouter(
         key: state.pageKey,
       ),
     ),
+
     GoRoute(
       path: RoutePaths.onboaringScreen,
       pageBuilder: (context, state) => CupertinoPage<void>(
@@ -24,10 +30,69 @@ final GoRouter routerConfig = GoRouter(
         key: state.pageKey,
       ),
     ),
+
     GoRoute(
       path: RoutePaths.selectUserProfileScreen,
       pageBuilder: (context, state) => CupertinoPage<void>(
         child: const SelectUserProfileScreen(),
+        key: state.pageKey,
+      ),
+    ),
+
+    GoRoute(
+      path: RoutePaths.signInScreen,
+      pageBuilder: (context, state){
+        if(state.extra != null){
+          Map<String, dynamic> args = state.extra as Map<String, dynamic>;
+          return CupertinoPage<void>(
+            child: SignInScreen(
+              userType: args['userType'] ?? UserType.user,
+            ),
+            key: state.pageKey,
+          );
+        }
+        return CupertinoPage<void>(
+        child: const SignInScreen(
+          userType: UserType.user,
+        ),
+        key: state.pageKey,
+      );
+      }
+    ),
+    
+    GoRoute(
+      path: RoutePaths.signUpScreen,
+      pageBuilder: (context, state){
+        if(state.extra != null){
+          Map<String, dynamic> args = state.extra as Map<String, dynamic>;
+          return CupertinoPage<void>(
+            child: SignUpScreen(
+              userType: args['userType'] ?? UserType.user,
+            ),
+            key: state.pageKey,
+          );
+        }
+        return CupertinoPage<void>(
+        child: const SignUpScreen(
+          userType: UserType.user,
+        ),
+        key: state.pageKey,
+      );
+      }
+    ),
+
+    GoRoute(
+      path: RoutePaths.verificationScreen,
+      pageBuilder: (context, state) => CupertinoPage<void>(
+        child: const VerificationScreen(),
+        key: state.pageKey,
+      ),
+    ),
+
+    GoRoute(
+      path: RoutePaths.forgotPasswordScreen,
+      pageBuilder: (context, state) => CupertinoPage<void>(
+        child: const ForgotPasswordScreen(),
         key: state.pageKey,
       ),
     ),
