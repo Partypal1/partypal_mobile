dfd7bd71601ad8cb988013afef573eea3aa76fe9
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2023-12-30 19:38:18 +0100

    created the verification screen

--- lib/configs/router_config.dart
+++ lib/configs/router_config.dart
@@ -1,19 +1,20 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:partypal/configs/route_paths.dart';
 import 'package:partypal/models/user_model.dart';
-import 'package:partypal/screens/authentication/forgot_password.dart';
+import 'package:partypal/screens/authentication/reset_password_screen.dart';
 import 'package:partypal/screens/authentication/select_user_profile_screen.dart';
 import 'package:partypal/screens/authentication/sign_in_screen.dart';
 import 'package:partypal/screens/authentication/sign_up_screen.dart';
-import 'package:partypal/screens/authentication/verification.dart';
+import 'package:partypal/screens/authentication/verification_screen.dart';
 import 'package:partypal/screens/onboarding_screen.dart';
 import 'package:partypal/screens/splash_screen.dart';
 
 final GoRouter routerConfig = GoRouter( // TODO: add routing animations
   initialLocation: RoutePaths.splashScreen,
-  errorBuilder: (context, state) => const Placeholder(),
+  errorBuilder: (context, state) => const ErrorBuilder(),
   routes: [
     GoRoute(
       path: RoutePaths.splashScreen,
@@ -52,11 +53,9 @@ final GoRouter routerConfig = GoRouter( // TODO: add routing animations
           );
         }
         return CupertinoPage<void>(
-        child: const SignInScreen(
-          userType: UserType.user,
-        ),
-        key: state.pageKey,
-      );
+          child: const ErrorBuilder(),
+          key: state.pageKey
+        );
       }
     ),
     
@@ -73,28 +72,55 @@ final GoRouter routerConfig = GoRouter( // TODO: add routing animations
           );
         }
         return CupertinoPage<void>(
-        child: const SignUpScreen(
-          userType: UserType.user,
-        ),
-        key: state.pageKey,
-      );
+          child: const ErrorBuilder(),
+          key: state.pageKey
+        );
       }
     ),
 
     GoRoute(
       path: RoutePaths.verificationScreen,
-      pageBuilder: (context, state) => CupertinoPage<void>(
-        child: const VerificationScreen(),
-        key: state.pageKey,
-      ),
+      pageBuilder: (context, state){
+        if (state.extra!=null){
+          Map<String, dynamic> args = state.extra as Map<String, dynamic>;
+          return CupertinoPage<void>(
+            child: VerificationScreen(
+              email: args['email'] ?? '',
+            ),
+            key: state.pageKey,
+          );
+        }
+        return CupertinoPage<void>(
+          child: const ErrorBuilder(),
+          key: state.pageKey
+        );
+      }
     ),
 
     GoRoute(
       path: RoutePaths.forgotPasswordScreen,
       pageBuilder: (context, state) => CupertinoPage<void>(
-        child: const ForgotPasswordScreen(),
+        child: const ResetPasswordScreen(),
         key: state.pageKey,
       ),
     ),
   ]
 );
+
+class ErrorBuilder extends StatelessWidget {
+  const ErrorBuilder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Navigation error has occoured',
+          style: Theme.of(context).textTheme.titleLarge?.copyWith(
+            color: Theme.of(context).colorScheme.error
+          ),
+        ),
+      ),
+    );
+  }
+}
