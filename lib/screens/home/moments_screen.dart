8b20b7648e99b8b9dd6c86e165a80a699b5363ab
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-01-26 00:10:55 +0100

    created the post, moment, and explore files

---
+++ lib/screens/home/moments_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MomentsScreen extends StatelessWidget {
+  const MomentsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+      body: Center(
+        child: Text('Moments'),
+      ),
+    );
+  }
+}
