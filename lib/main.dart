92467b33f1cd563aac8cbc7835c50eff94d9c598
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-02-20 16:18:44 +0100

    added the multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:partypal/configs/router_config.dart';
 import 'package:partypal/configs/supported_locales.dart';
+import 'package:partypal/services/auth_provider.dart';
+import 'package:partypal/services/category_provider.dart';
+import 'package:partypal/services/event_provider.dart';
+import 'package:partypal/services/moment_provider.dart';
 import 'package:partypal/theme/theme.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   ScreenUtil.ensureScreenSize();
@@ -15,15 +20,23 @@ class Partypal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context);
-    return MaterialApp.router(
-      title: 'Partypal',
-      theme: theme,
-      darkTheme: darkTheme,
-      themeMode: ThemeMode.system,
-      debugShowCheckedModeBanner: false,
-      routerConfig: routerConfig,
-      supportedLocales: supportedLocales,
-      localizationsDelegates: localizationsDelegates
+    return MultiProvider(
+      providers: [
+        Provider<AuthProider>(create: (_) => AuthProider()),
+        Provider<CategoryProvider>(create: (_) => CategoryProvider()),
+        Provider<EventProvider>(create: (_) => EventProvider()),
+        Provider<MomentProvider>(create: (_) => MomentProvider())
+      ],
+      child: MaterialApp.router(
+        title: 'Partypal',
+        theme: theme,
+        darkTheme: darkTheme,
+        themeMode: ThemeMode.system,
+        debugShowCheckedModeBanner: false,
+        routerConfig: routerConfig,
+        supportedLocales: supportedLocales,
+        localizationsDelegates: localizationsDelegates
+      ),
     );
   }
 }
