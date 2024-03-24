6253bef7dbcaa85944b3679656d6099c84ba851c
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-03-24 15:48:49 +0100

    added support for prefix icons

--- lib/widgets/buttons/outlined_button.dart
+++ lib/widgets/buttons/outlined_button.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomOutlinedButton extends StatelessWidget {
   final String label;
   final VoidCallback? onTap;
   final Color? color;
+  final IconData? iconData;
   const CustomOutlinedButton({
     required this.label,
     this.onTap,
     this.color,
+    this.iconData,
     super.key});
 
   @override
@@ -26,13 +29,32 @@ class CustomOutlinedButton extends StatelessWidget {
               
             ),
             child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
+              padding: iconData == null
+                ? const EdgeInsets.symmetric(horizontal: 24)
+                : const EdgeInsets.only(left: 16, right: 24),
               child: Center(
-                child: Text(
-                  label,
-                  style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                    color: color ?? Theme.of(context).colorScheme.onSurface
-                  )
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    iconData != null
+                    ? Icon(
+                      iconData,
+                      size: 18,
+                      color: color ?? Theme.of(context).colorScheme.onSurface,
+                    )
+                    : const SizedBox.shrink(),
+
+                    iconData != null
+                    ? 8.horizontalSpace
+                    : const SizedBox.shrink(),
+
+                    Text(
+                      label,
+                      style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                        color: color ?? Theme.of(context).colorScheme.onSurface
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
