4d5f6325fb083d1756f5ca2077d7de5dfd06538b
olasunkanmi beckley <beckleysunkanmi@gmail.com>
2024-04-15 22:58:19 +0100

    created place screen and the heat map card

--- lib/screens/home/place_screen.dart
+++ lib/screens/home/place_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_lazy_indexed_stack/flutter_lazy_indexed_stack.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:partypal/models/place_model.dart';
 import 'package:partypal/widgets/app_bars/app_bar.dart';
+import 'package:partypal/widgets/cards/circle_image.dart';
+import 'package:partypal/widgets/cards/place_heat_map_card.dart';
+import 'package:partypal/widgets/others/placeholders.dart';
 
 class PlaceScreen extends StatefulWidget {
   final Place place;
@@ -12,7 +18,24 @@ class PlaceScreen extends StatefulWidget {
   State<PlaceScreen> createState() => _PlaceScreenState();
 }
 
-class _PlaceScreenState extends State<PlaceScreen> {
+class _PlaceScreenState extends State<PlaceScreen> with SingleTickerProviderStateMixin{
+  late TabController _tabController;
+
+  @override
+  void initState(){
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose(){
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +43,109 @@ class _PlaceScreenState extends State<PlaceScreen> {
       body: CustomScrollView(
         slivers: [
           SliverPersistentHeader(
-            delegate: SliverCustomAppBarDelegate(title: widget.place.name),
-          )
+            delegate: CustomSliverAppBar(title: widget.place.name),
+            pinned: true,
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: SizedBox(
+                height: 80,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    CircleImage(imageUrl: widget.place.imageUrl, radius: 40,),
+                    10.horizontalSpace,
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        TextPlaceHolder(height: 14, width: 100),
+                        TextPlaceHolder(height: 14, width: 120),
+                        TextPlaceHolder(height: 14, width: 80),
+                        // Text(
+                        //   '',
+                        //   style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                        //     fontWeight: FontWeight.bold
+                        //   ),
+                        // ),
+                        // Text(
+                        //   '',
+                        //   style: Theme.of(context).textTheme.bodySmall
+                        // ),
+                        // Text(
+                        //   '',
+                        //   style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                        //     fontWeight: FontWeight.bold,
+                        //     color: Theme.of(context).colorScheme.secondary
+                        //   ),
+                        // ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SliverPersistentHeader(delegate: _PlaceTab(tabController: _tabController)),
+          SliverToBoxAdapter(
+            child: LazyIndexedStack(
+              index: _tabController.index,
+              children: [
+                PlaceHeatMapCard(place: widget.place),
+                const SizedBox(),
+                const SizedBox(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _PlaceTab extends SliverPersistentHeaderDelegate{
+  final TabController tabController;
+  _PlaceTab({
+    required this.tabController,
+  });
+
+  @override
+  double get minExtent => 80;
+
+  @override
+  double get maxExtent => 80;
+
+  @override
+  bool shouldRebuild(_PlaceTab oldDelegate){
+    return false;
+  }
+  @override
+  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent){
+    return SizedBox(
+      height: 80,
+      child: TabBar(
+        controller: tabController,
+        splashBorderRadius: BorderRadius.circular(4),
+        tabs: const [
+          SizedBox(
+            height: 50,
+            child: Center(
+              child: Text('Overview'),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            child: Center(
+              child: Text('Events'),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            child: Center(
+              child: Text('Menu'),
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/widgets/cards/place_heat_map_card.dart
@@ -0,0 +1,296 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:partypal/models/place_model.dart';
+import 'package:partypal/widgets/others/tonal_elevation.dart';
+import 'dart:math' as math;
+
+class PlaceHeatMapCard extends StatefulWidget {
+  final Place place;
+  const PlaceHeatMapCard({
+    required this.place,
+    super.key});
+
+  @override
+  State<PlaceHeatMapCard> createState() => _PlaceHeatMapCardState();
+}
+
+class _PlaceHeatMapCardState extends State<PlaceHeatMapCard>{
+  late List<List<double>> heatMap;
+  int selectedDayIndex = DateTime.now().weekday;
+
+  @override
+  void initState(){
+    super.initState();
+    heatMap = _generateHeatMap();
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+  }
+
+  List<List<double>> _generateHeatMap(){ //TODO: get the actual heat map from place model
+    final random = math.Random();
+    return List.generate(7, (index){
+      return List.generate(8, (index) => (random.nextInt(7) + 2) / 10);
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface.tonalElevation(Elevation.level1, context)
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            0.03.sw.verticalSpace,
+
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: Theme.of(context).colorScheme.surface.tonalElevation(Elevation.level2, context)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                      child: Text(
+                        'Heat map',
+                        style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            0.03.sw.verticalSpace,
+
+            _DaySelector( // day selector
+              onTap: (value){
+                setState(() {
+                  selectedDayIndex = value;
+                });
+              }
+            ),
+
+            0.03.sw.verticalSpace,
+
+            Center( // graph
+              child: SizedBox.square(
+                dimension: 0.9.sw,
+                child: CustomPaint(
+                  painter: _GraphPainter(
+                    context,
+                    heatMapForTheDay: heatMap[selectedDayIndex]
+                  ),
+                ),
+              ),
+            ),
+
+            0.03.sw.verticalSpace,
+
+            Row( // legend
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.surface.tonalElevation(Elevation.level2, context),
+                        borderRadius: BorderRadius.circular(8)
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                SizedBox.square(
+                                  dimension: 10,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.red,
+                                      borderRadius: BorderRadius.circular(5)
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  '  Hot',
+                                  style: Theme.of(context).textTheme.labelLarge,
+                                ),
+                                Text(
+                                  '  high enegy',
+                                  style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                                    color: Theme.of(context).colorScheme.outline.withOpacity(0.5)
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                SizedBox.square(
+                                  dimension: 10,
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                      color: Colors.blue,
+                                      borderRadius: BorderRadius.circular(5)
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  '  Chill',
+                                  style: Theme.of(context).textTheme.labelLarge,
+                                ),
+                                Text(
+                                  '  low enegy',
+                                  style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                                    color: Theme.of(context).colorScheme.outline.withOpacity(0.5)
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            0.03.sw.verticalSpace
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DaySelector extends StatefulWidget {
+  final ValueChanged<int>? onTap;
+  const _DaySelector({
+    this.onTap,
+    super.key
+  });
+
+  @override
+  State<_DaySelector> createState() => _DaySelectorState();
+}
+
+class _DaySelectorState extends State<_DaySelector> {
+  List<String> days = ['su', 'mo' ,'tu' ,'we', 'th', 'fr', 'sa'];
+  int selectedDayIndex = DateTime.now().weekday;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: List.generate(
+        days.length,
+        (index){
+          return GestureDetector(
+            onTap: (){
+              setState(() {
+                selectedDayIndex = index;
+              });
+              widget.onTap?.call(index);
+            },
+            child: SizedBox(
+              width: 0.1.sw,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(0.05.sw),
+                  color: selectedDayIndex == index
+                    ? Theme.of(context).colorScheme.onSurface
+                    : null,
+                  border: selectedDayIndex == index
+                    ? null
+                    : Border.all(
+                        width: 0.5,
+                        color: Theme.of(context).colorScheme.onSurface.withOpacity(0.5)
+                      )
+                ),
+                child: Center(
+                  child: Text(
+                    days[index],
+                    style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                      color: selectedDayIndex == index
+                        ? Theme.of(context).colorScheme.onInverseSurface
+                        : Theme.of(context).colorScheme.onSurface
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        }
+      )
+    );
+  }
+}
+
+class _GraphPainter extends CustomPainter{
+  final List<double> heatMapForTheDay;
+  final BuildContext context;
+  const _GraphPainter( this.context, {
+    required this.heatMapForTheDay
+  });
+
+  Color _interpolateColor(Color color1, Color color2, double value){
+    return Color.fromRGBO(
+      color1.red + ((color2.red - color1.red) * value).floor(),
+      color1.green + ((color2.green - color1.green) * value).floor(),
+      color1.blue + ((color2.blue - color1.blue) * value).floor(),
+      1
+    );
+  }
+
+  @override
+  bool shouldRepaint(_GraphPainter oldDelegate) => oldDelegate.heatMapForTheDay != heatMapForTheDay;
+
+  @override
+  void paint(canvas, size){
+    for (int i = 0; i <= heatMapForTheDay.length; i++){ // draw grid
+      canvas.drawLine(
+        Offset(0, size.height * i/heatMapForTheDay.length),
+        Offset(size.width, size.height * i/heatMapForTheDay.length),
+        Paint()
+          ..color = Theme.of(context).colorScheme.outline.withOpacity(0.5)
+          ..strokeCap = StrokeCap.round
+      );
+      canvas.drawLine(
+        Offset(size.width * i/heatMapForTheDay.length, 0),
+        Offset(size.width * i/heatMapForTheDay.length, size.height),
+        Paint()
+          ..color = Theme.of(context).colorScheme.outline.withOpacity(0.5)
+          ..strokeCap = StrokeCap.round
+      );
+    }
+
+    for (int i = 0; i < heatMapForTheDay.length; i++){ // draw points
+      canvas.drawCircle(
+        Offset(
+          size.width * i/heatMapForTheDay.length + (size.width/ (2 * heatMapForTheDay.length)),
+          size.height * (1 - heatMapForTheDay[i])
+        ),
+        5,
+        Paint()
+          ..color = _interpolateColor(Colors.blue, Colors.red, heatMapForTheDay[i])
+      );
+    }    
+  }
+}
